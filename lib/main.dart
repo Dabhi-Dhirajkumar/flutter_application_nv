424e3c86cf31cec6f17b66b22739a2bccdc57d81
Dabhi-Dhirajkumar <dhirajjdabhi1@gmail.com>
2025-07-29 19:15:47 +0530

    Flutter project upload

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+//import 'package:flutter_application_nvs/1507flutter/basiccompo.dart';
+//import 'package:flutter_application_nvs/1507flutter/columnexample.dart';
+//import 'package:flutter_application_nvs/1507flutter/textstyleexmp.dart';
+import'package:flutter_application_nv/2207flutter/stopwatch.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Stopwatch(),
+      );
+  }
+}
